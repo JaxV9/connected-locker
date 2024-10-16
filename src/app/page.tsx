9f8eea26848f8142c36987830a7c423932fc4f52
JaxV9 <layanj.pro@gmail.com>
2024-10-16 15:29:02 +0200

    feat: locker-list component added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,63 @@
+import { Card } from "./components/ui/card/card";
+import { CardList } from "./components/ui/cardList/cardList";
+import { LockerType } from "./model/locker";
 
 
 export default function Home() {
+
+  const lockerList: LockerType[] = [
+    {
+      id: 1,
+      user: 1,
+      state: "assigned"
+    },
+    {
+      id: 2,
+      user: null,
+      state: "available"
+    },
+    {
+      id: 3,
+      user: 1,
+      state: "assigned"
+    },
+    {
+      id: 4,
+      user: null,
+      state: "available"
+    },
+    {
+      id: 5,
+      user: 1,
+      state: "assigned"
+    },
+    {
+      id: 6,
+      user: null,
+      state: "available"
+    },
+    {
+      id: 7,
+      user: 1,
+      state: "assigned"
+    },
+    {
+      id: 8,
+      user: null,
+      state: "available"
+    }
+  ]
   return (
-    <h1>Connected locker</h1>
+    <>
+      <h1>Connected locker</h1>
+
+      <CardList >
+        {lockerList.length > 0 ?
+          lockerList.map((locker) => (
+            <Card key={locker.id} id={locker.id} user={locker.user} state={locker.state} />
+          ))
+          : null}
+      </CardList>
+    </>
   );
 }
